ee9f4e8cc644a29df3840d6922a910fcf79e0f37
GER0004 <egor201295@gmail.com>
2025-09-10 20:17:41 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true, // включаем поддержку директории /app
+  },
+  output: 'standalone', // чтобы Vercel собирал правильно
 };
+
 module.exports = nextConfig;
